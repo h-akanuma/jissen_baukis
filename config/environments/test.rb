05c203f66529dd19336b9770a9068dc96000ec86
Hiroaki Akanuma <hiroaki.akanuma@nubee.com>
2014-10-28 13:45:58 +0900

    Until 1357

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,6 +41,6 @@ Rails.application.configure do
     staff: { host: 'baukis.example.com', path: '' },
     admin: { host: 'baukis.example.com', path: 'admin' },
     customer: { host: 'example.com', path: 'mypage' },
-    restrict_ip_addresses: true
+    restrict_ip_addresses: false
   }
 end
